5c7ce5b669a551156d36ff62e7cef045d11a4c3d
Kartikey <kmishr11@in.ibm.com>
2018-07-17 10:48:15 +0530

    fixed QuadRoot

--- Exercise 2/Q2/src/QuadRoot.java
+++ Exercise 2/Q2/src/QuadRoot.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 
 public class QuadRoot {
+    private double quadRoot;
+
+    public boolean isPowerOf4(int number) {
+        quadRoot = Math.pow(number, 1.0 / 4.0);
+        if (number % quadRoot == 0) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        QuadRoot qr = new QuadRoot();
 
         System.out.println("Enter a number: ");
-        int number = scanner.nextInt();
+        int input = scanner.nextInt();
 
-        double quadRoot = Math.pow(number, 1.0 / 4.0);
-
-        if (number % quadRoot == 0) {
-            System.out.println("The number is a power of 4");
+        if (qr.isPowerOf4(input)) {
+            System.out.println("Number is a power of 4");
         } else {
-            System.out.println("The number is not a power of 4");
+            System.out.println("Number is not a power of 4");
         }
     }
 }
