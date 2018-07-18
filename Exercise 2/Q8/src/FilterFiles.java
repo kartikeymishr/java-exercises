d4be97f6e9009e265d7c1d50191772efa972ef38
Kartikey <kmishr11@in.ibm.com>
2018-07-18 11:44:24 +0530

    fixed FilterFiles for byte array

--- Exercise 2/Q8/src/FilterFiles.java
+++ Exercise 2/Q8/src/FilterFiles.java
@@ -1,22 +1,53 @@
-import java.io.File;
+import java.io.*;
 
 public class FilterFiles {
-
-    public void listing(File[] listOfFiles, String searchString) {
+    public void listing(File[] listOfFiles, String searchString) throws FileNotFoundException {
         for (File file : listOfFiles) {
             if (file.isDirectory()) {
                 listing(file.listFiles(), searchString);
             } else if (file.getName().contains(searchString)) {
                 System.out.println(file.getName());
+                readContents(file);
+            }
+        }
+    }
+
+    public void readContents(File file) throws FileNotFoundException {
+        FileInputStream stream = null;
+
+        try {
+            stream = new FileInputStream(file);
+            byte fileContent[] = new byte[(int) file.length()];
+            stream.read(fileContent);
+            String s = new String(fileContent);
+            System.out.println("File content: ");
+            System.out.println(s);
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: File not found");
+            e.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println("ERROR: File could not be read");
+            ioe.printStackTrace();
+        } finally {
+            try {
+                if (stream != null) {
+                    stream.close();
+                }
+            } catch (IOException ioe) {
+                System.out.println("ERROR: There was a problem in closing the stream");
+                ioe.printStackTrace();
             }
         }
     }
 
     public static void main(String[] args) {
-        // Search folder
         File folder = new File("/home/kartikey/Desktop");
         File[] listOfFiles = folder.listFiles();
         FilterFiles ff = new FilterFiles();
-        ff.listing(listOfFiles, ".xml");
+        try {
+            ff.listing(listOfFiles, ".java");
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: File not found");
+        }
     }
 }
